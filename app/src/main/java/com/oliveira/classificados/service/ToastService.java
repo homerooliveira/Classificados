0bf3077b7aec88f28992a5084f1396bba099042e
Homero Oliveira <homero_junior_oliveira@outlook.com>
2017-01-17 19:20:24 -0200

    Using handler for queue of android.

--- app/src/main/java/com/oliveira/classificados/service/ToastService.java
+++ app/src/main/java/com/oliveira/classificados/service/ToastService.java
@@ -2,6 +2,9 @@ package com.oliveira.classificados.service;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v4.content.LocalBroadcastManager;
 import android.widget.Toast;
 
@@ -20,22 +23,44 @@ public class ToastService extends IntentService {
 
         final String msg = intent.getStringExtra(KEY_MSG);
 
+        final Handler handler = new Handler(Looper.getMainLooper()) {
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                String msgTxt = (String) msg.obj;
+                Toast.makeText(ToastService.this, msgTxt, Toast.LENGTH_SHORT).show();
+            }
+        };
+
         new Thread(new Runnable() {
             @Override
             public void run() {
 
+                int i = 0;
                 while (!Thread.interrupted()) {
+                    i++;
+
                     try {
                         Thread.sleep(5 * 1000); // 5 seg
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
 
-                    Intent intentBrod = new Intent(ACTION_FILTER);
-                    intentBrod.putExtra(KEY_MSG, msg);
+                    //Esta versão utiliza a fila do android
+                    Message message = new Message();
+                    message.obj = String.format("%s: %s", msg, i);
+                    // Este método envia para fila do android
+                    handler.sendMessage(message);
 
-                    LocalBroadcastManager.getInstance(ToastService.this)
-                            .sendBroadcast(intentBrod);
+                    //Local Broadcast
+//
+//                    Intent intentBrod = new Intent(ACTION_FILTER);
+//                    intentBrod.putExtra(KEY_MSG, msg);
+//
+//                    LocalBroadcastManager.getInstance(ToastService.this)
+//                            .sendBroadcast(intentBrod);
                 }
             }
         }).start();
